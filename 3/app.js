fa273dab303c7b1e453463120f19796426c90568
Marina Pasternak <256pasternakmarina@gmail.com>
2020-05-13 23:38:33 +0300

    assignement_3

---
+++ 3/app.js
@@ -0,0 +1,93 @@
+(function (){
+    'use strict';
+    angular.module('NarrowItDownApp',[])
+    .controller('NarrowItDownController',NarrowItDownController)
+    .service('MenuSearchService',MenuSearchService)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+    
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var menu = this;
+        
+        menu.showItems=function()
+        {
+            let searchTerm = menu.searchTerm
+            menu.categories=MenuSearchService.getMatchedMenuItems(searchTerm);
+            menu.massage=MenuSearchService.massage();
+        }
+        menu.delItems=function(index)
+        {
+            MenuSearchService.delItem(index);
+            
+        }
+       
+        
+    }
+    MenuSearchService.$inject=['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath)
+    {
+        let service = this;
+        let arr=[];
+        let findArr=[]
+        function getList()
+        {
+            let response = $http({
+                method: "GET",
+                url: (ApiBasePath + "/categories.json")
+              });
+          
+            return response;
+            
+        }
+
+        let promise = getList();
+        promise.then(function (response) {
+           arr =response.data;
+           console.log(arr);
+        })
+        .catch(function (error) {
+            console.log("Something went terribly wrong.");
+        });
+
+        service.getMatchedMenuItems=function(searchTerm)
+        {
+            findArr=[];
+            let regEx=new RegExp(searchTerm);
+            
+            console.log(searchTerm);
+            for(let i=0;i<arr.length;i++)
+            {
+                if(arr[i]['name'].match(regEx) || arr[i]['special_instructions'].match(regEx))
+                    {
+                        console.log(arr[i]['name'].match(regEx)+"----"+arr[i]['special_instructions'].match(regEx))
+                        findArr.push(arr[i])
+                    }
+                    
+            }
+           
+            console.log(findArr)
+            return findArr
+        }
+        
+        service.delItem=function(index)
+        {
+            findArr.splice(index,1);
+            return findArr;
+        }
+
+
+        service.massage=function(){
+            if(findArr.length==0)
+            {
+                return 'Nothing found!';
+            }
+            else
+            {
+                return 'Have found!';
+            }
+        }
+            
+        
+    }
+
+})();
